b4b024eef7f4dd6cfa4f1eb4656b78daeac716ed
Willian7004 <128359604+Willian7004@users.noreply.github.com>
2025-02-11 19:52:12 +0800

    清理过时内容

--- ai-note/pages/LLM使用案例.py
+++ ai-note/pages/LLM使用案例.py
@@ -17,43 +17,8 @@ with open("ai-note/files/题目3.txt", "r", encoding='utf-8') as f:
 with st.container(height=240):
     st.write(t3)
 st.subheader("2.编程", divider=True)
-st.write("如果有详细的提示词，非思维链模型可以编写大部分程序，使用思维链模型可以适度精简提示词或改为描述功能。我在https://william7004-homepage.streamlit.app 提及的我的github仓库中包含提示词的为使用LLM辅助编写的程序。大多使用deepseek coder v2及之后的deepseek模型。")
+st.write("如果有详细的提示词，非思维链模型可以编写大部分程序，使用思维链模型可以适度精简提示词或改为描述功能。我的github仓库中包含llm辅助编程的对话记录，除嵌入式和c语言外大部分仓库包含llm辅助编写的程序。")
 st.subheader("3.短文写作", divider=True)
-st.write("我部署LLM后经常使用介绍一个地点的指令进行测试，这部分的文章也来源于这项流程。个人感觉Qwen2 72b在写作上比较优秀，后面的模型引入了不少指令数据后反而导致在写作上缺少亮点。")
-with open("ai-note/files/qwen2底特律.txt", "r", encoding='utf-8') as f:
-            t4= f.read()
-with st.container(height=240):
-    st.write(t4)
-with open("ai-note/files/qwen2威尼斯.txt", "r", encoding='utf-8') as f:
-            t5= f.read()
-with st.container(height=240):
-    st.write(t5)
-with open("ai-note/files/qwen2乌鲁木齐.txt", "r", encoding='utf-8') as f:
-            t6= f.read()
-with st.container(height=240):
-    st.write(t6)
-st.subheader("4.长文写作", divider=True)   
-st.write("使用longwriter模型可以直接生成长文，个人感觉longwriter GLM4 9b效果相对较好，但由于参数量较小，实际效果不太好。")
-with open("ai-note/files/longwriter glm4 9b法国介绍.txt", "r", encoding='utf-8') as f:
-            t7= f.read()
-with st.container(height=240):
-    st.write(t7)
-with open("ai-note/files/longwriter glm4 9b曲速航天 .txt", "r", encoding='utf-8') as f:
-            t8= f.read()
-with st.container(height=240):
-    st.write(t8)    
-st.write("对于各部分关联较小的游记等文章，可以生成摘要并分别生成各部分，以下文章使用deepseek v2生成。")
-with open("ai-note/files/deepseek-v2美国游记.txt", "r", encoding='utf-8') as f:
-            t9= f.read()
-with st.container(height=240):
-    st.write(t9)
-with open("ai-note/files/deepseek-v2法国游记.txt", "r", encoding='utf-8') as f:
-            t10= f.read()
-with st.container(height=240):
-    st.write(t10)
-st.write("对于小说等各部分关联较大的文章，需要向模型输入上下文和各部分摘要，以下文章使用上述的小说agent项目调用deepseek v2.5生成，目前该项目仍不完善，生成时部分章节内容重复。")
-with open("ai-note/files/极速梦想.txt", "r", encoding='utf-8') as f:
-            t11= f.read()
-with st.container(height=240):
-    st.write(t11)
+st.write("deepseek r1使用了纯rl，在写作上有较大优势。相关内容放到articles-by-llm部分。")
+
 st.show()
--- ai-note/pages/其它模型.py
+++ ai-note/pages/其它模型.py
@@ -3,11 +3,11 @@ import streamlit as st
 st.title("其它模型")
 st.write("在前面提到的模型类型以外，这部分介绍一些我使用的其它一些模型类型，大多是规模比较小的模型。")
 st.subheader("1.TTS模型", divider=True)
-st.write("TTS模型用于语音合成。目前没有比较好的可以图形化运行TTS模型的运行框架，以前我主要使用一些模型的第三方整合包，后来看到Fish Speech可以通过官方代码以整合包形式快速部署，支持api，官方还做了Windows版本的GUI应用，生成时能按句子进行分割，就在TTS模型上改用Fish Speech。个人感觉生成虽然不太真实但也算是有比较好的效果。以下音频使用Fish Speech生成。")
-st.audio("ai-note/files/fish-speech.mp3", format="audio/mpeg", loop=False)
+st.write("TTS模型用于语音合成。目前没有比较好的可以图形化运行TTS模型的运行框架，以前我主要使用一些模型的第三方整合包，后来看到Fish Speech可以通过官方代码以整合包形式快速部署，支持api，官方还做了Windows版本的GUI应用，生成时能按句子进行分割，就在TTS模型上改用Fish Speech。虽然不完全真实但效果优于大部分同类模型，aeficles-by-llm页面包含使用fish speech生成的内容。")
+
 st.subheader("2.图像识别/语义分割模型", divider=True)
 st.write("这是比较成熟的模型类型，不太旧的模型都有较高的准确度。新的模型的主要方向是开放词汇识别，也就是可以识别任意类型的物体。目前在ComfyUI通过YOLO World Efficient SAM部署了YOLO World和SAM实现图片分割。此外，多模态模型可以用于这类任务，速度较慢但可以生成场景描述，适合用于自然语言图片标注。")
 st.subheader("3.音乐模型", divider=True)
-st.write("目前Suno等闭源音乐模型表现还可以，而开源音乐模型比较常用的只有Stable Audio Open，生成效果一般并且成功率低。以下为Stable Audio Open生成的音频。")
-st.audio("ai-note/files/stable-audio.mp3", format="audio/mpeg", loop=False)
+st.write("开源音乐模型效果最好的是Yue s1，与Suno等商业模型效果相当。")
+
 st.show()
